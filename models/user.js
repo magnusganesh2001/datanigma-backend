df71c6ad6bd728dfa62b61f94702561f36b53e80
Ganesh J <91062187+magnusganesh2001@users.noreply.github.com>
2022-11-19 16:38:41 +0530

    update url

--- models/user.js
+++ models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = mongoose.Schema({
+<<<<<<< HEAD
     name: { type: String, required: true },
     company: { type: String },
     companyUrl: { type: String },
@@ -15,6 +16,20 @@ const userSchema = mongoose.Schema({
     technologies: [{ type: String }],
     location: { type: String },
     // currentPosition: { type: String }
+=======
+  name: { type: String, required: true},
+  company: { type: String },
+  companyUrl : { type: String, required: true},
+  phone: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  type: { type: String, required: true },
+  resume: { type: String },
+  languagesKnown: { type: String },
+  skills: [{ type: String }],
+  technologies: [{ type: String }],
+  location: { type: String }
+>>>>>>> 7007143 (update url)
 });
 
 userSchema.plugin(uniqueValidator);
